697db5f662a319bdfbbb00050103d8824caf0d68
dimalesnik <37703619+dimalesnik@users.noreply.github.com>
2018-07-13 17:54:42 +0300

    Add files via upload

---
+++ 13.07.18/dom.js
@@ -0,0 +1,15 @@
+document.getElementById('container').innerHTML = '<div class="row">Content Text</div>'; // Parsing, 
+
+// elements
+document.createElement('tag') -> object
+parent.appendChild( element ) - adaugarea obiectului
+parent.insertBefore( element, referenceElement);
+parent.removeChild( element ) - stergerea obiectului
+parent.replaceChild( element, oldElement)
+
+// atributes
+// standart .src, .style, .width, .href,
+// .className -> class=""
+element.setAttribute('name','value')
+element.getAttribute('name')
+element.removeAttribute('name')
---
+++ 13.07.18/mini-tank/tank.js
@@ -0,0 +1,36 @@
+function createTank(){ // creaza un div cu classa Tank si il plaseaza in interiorul div'ului cu classa road
+    var tank = document.createElement('div');   // <div></div>
+        tank.className = 'tank'                 // <div class="tank"></div>
+    var road = document.getElementById('road');
+        road.appendChild( tank );
+        return tank;
+}
+
+var tank = createTank();
+var left = 0;
+
+function createTree(){
+    var tree = document.createElement('div');
+        tree.className = 'tree'
+    var road = document.getElementById('road');
+        road.appendChild( tree );
+        return tree;
+}
+
+var tree = createTree();
+
+function moveTank(){ // event < capturam
+    console.log( event.keyCode );
+    if( event.keyCode == 37 ){
+        // alert('move left')
+        left--
+        tank.style.left = left + 'px';
+        tank.className='tank left';
+    }
+    if( event.keyCode == 39 ){
+        // alert('move right')
+        left++;
+        tank.style.left = left + 'px';
+        tank.className='tank right';
+    }
+}
